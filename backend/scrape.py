bb0a682fdfbf9defd86964db65fa8d095f58ea13
Ubuntu <ubuntu@ip-172-26-12-7.ap-northeast-1.compute.internal>
2018-10-10 06:48:16 +0000

    chrome

--- backend/scrape.py
+++ backend/scrape.py
@@ -7,6 +7,9 @@ import os.path
 from selenium import webdriver
 # from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from glob import glob
 
 import db
@@ -29,11 +32,14 @@ def generate_op(code):
     path = os.getcwd() + "/backend/data/%s" % code
     if not os.path.isdir(path):
         os.mkdir(path)
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-extensions")
+    options = Options()
+    options.binary_location = '/usr/bin/google-chrome'
+    options.add_argument('--headless')
+    options.add_argument('--window-size=1280,1024')
+    #chrome_options.add_argument("--disable-extensions")
     prefs = {"download.default_directory" : path}
-    chrome_options.add_experimental_option("prefs",prefs)
-    chrome_options.add_argument('--ignore-certificate-errors')     # fp = webdriver.FirefoxProfile()
+    options.add_experimental_option("prefs",prefs)
+    #chrome_options.add_argument('--ignore-certificate-errors')     # fp = webdriver.FirefoxProfile()
     # # 0:デスクトップ、1:システム規定のフォルファ、2:ユーザ定義フォルダ
     # fp.set_preference("browser.download.folderList",2)
     # # 上記で2を選択したのでファイルのダウンロード場所を指定
@@ -41,19 +47,25 @@ def generate_op(code):
     # fp.set_preference("browser.download.manager.showWhenStarting",False)
     # # mimeタイプを設定
     # fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
-    return chrome_options
+    return options
 
 
 def download(code):
-    time.sleep(5)
+    #time.sleep(5)
     op = generate_op(code)
     # driver = webdriver.Firefox(firefox_profile=fp,firefox_binary=binary)
     driver = webdriver.Chrome(chrome_options=op)
     url = base % (code,)
     print(url)
     driver.get(url)
+    driver.save_screenshot(os.getcwd() + '/screenshot1.png')
+    element = WebDriverWait(driver, 10).until(
+      EC.presence_of_element_located((By.CLASS_NAME, "resultTable"))
+    )
+    driver.save_screenshot(os.getcwd() + '/screenshot2.png')
     for ele in driver.find_elements_by_xpath('//table[@class="resultTable table_cellspacing_1 table_border_1 mb_6"]//tr/td[7]//a'):
-        time.sleep(1)
+        #time.sleep(1)
+        print(ele)
         ele.click()
     driver.close()
 
