6e2b19d185bd5ef094a1c25df915c87cd02c0902
temy13 <temy.poporofier00@gmail.com>
2018-10-12 10:51:15 +0900

    edinet

--- sample3.py
+++ sample3.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 
 options = Options()
 options.add_argument('--headless')
@@ -7,5 +8,8 @@ options.add_argument('--disable-gpu')
 options.add_argument('--window-size=1280,1024')
 driver = webdriver.Chrome(chrome_options=options)
 driver.get('https://disclosure.edinet-fsa.go.jp/W00Z0020.html')
+time.sleep(60)
 print(driver.title) #=> Google
 driver.save_screenshot('test.png')
+
+driver.quit()
