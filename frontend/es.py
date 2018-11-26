f6885d7fb2b41768bcf29f587d1264f6c8a5baa0
masahiro.takigawa02 <masahiro.takigawa02@g.softbank.co.jp>
2018-11-26 15:36:50 +0900

    indexes

--- frontend/es.py
+++ frontend/es.py
@@ -4,7 +4,7 @@ import json
 import helper
 conf = {"host": "127.0.0.1", "port": 9200,
          "index": "edinet", "doc_type": "edinet"}
- 
+import copy
 SIZE = 100
 
 def normal_search(_bool, offset):
@@ -42,7 +42,7 @@ def normal_search(_bool, offset):
             }
          }
       }
-        
+
     }
     d = es.search(index=conf["index"], body=body_)
     print(d["hits"]["total"], sum([x["doc_count"] for x in d['aggregations']['top_tags']['buckets']]))
@@ -74,7 +74,7 @@ def scroll_search(_bool, offset):
         break
     count = d["hits"]["total"]
     return count, result[offset:offset+SIZE]
-    
+
 
 
 def search(query, t_from="", t_to="", offset=0, titles=[]):
@@ -83,7 +83,7 @@ def search(query, t_from="", t_to="", offset=0, titles=[]):
     q_titles = str(tuple(titles)).replace(',)',')')
     offset = int(offset)
     filters =[
-    #{ 
+    #{
     #	"wildcard": {
     #	  "value":{
     #	     "value":"*"+query+"*"
@@ -98,7 +98,7 @@ def search(query, t_from="", t_to="", offset=0, titles=[]):
     {
     	"range": {
            "term_date_range": {
-            
+
                "gte":t_from,
                "lte":t_to,
 	       "format": "yyyy/MM/dd",
@@ -109,7 +109,9 @@ def search(query, t_from="", t_to="", offset=0, titles=[]):
     ]
     if titles:
        idx = helper.get_es_indexes(titles)
-       print(titles, idx)
+       indexes = copy.deepcopy(idx)
+       for i in idx:
+           indexes.extend(helper.TITLES_SUB_INDEXES[i])
        filters.append({
         "terms": {
             "title_index": [str(i) for i in idx]
@@ -122,11 +124,10 @@ def search(query, t_from="", t_to="", offset=0, titles=[]):
     else:
       count, result = scroll_search(_bool, offset)
     result = [{
-	"value":row["_source"]["value"], "publisher":row["_source"]["publisher"], "term":row["_source"]["term"], 
+	"value":row["_source"]["value"], "publisher":row["_source"]["publisher"], "term":row["_source"]["term"],
 	"term_from":row["_source"]["term_date_range"]["gte"], "term_to":row["_source"]["term_date_range"]["lte"],
         "title_index":row["_source"]["title_index"],
         "filename":row["_source"]["filename"]
                  # "title1":row["_source"]["title1"], "title2":row["_source"]["title2"], "title3":row["_source"]["title3"], "title4":row["_source"]["title4"], "title5":row["_source"]["title5"]
 	} for row in result]
     return count, result
-
